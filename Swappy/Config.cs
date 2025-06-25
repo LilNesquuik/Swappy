c474af198160bff4706d94f09e3523c01a185b9a
Morgan Coyaux <zmentionb@gmail.com>
2025-06-25 02:35:35 +0200

    😟

--- Swappy/Config.cs
+++ Swappy/Config.cs
@@ -24,6 +24,7 @@ public class Config
             RepositoryOwner = "LilNesquuik",
             RepositoryName = "Swappy",
             AccessToken = null,
+            DownloadDependencies = true,
             UpdateOnStartup = true,
             SoftRestart = true
         }
