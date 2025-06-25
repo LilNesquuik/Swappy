d75062e1e6c390faaaccd2d95b2d3222f1b1bdd4
Morgan Coyaux <zmentionb@gmail.com>
2025-06-25 03:31:41 +0200

    💬

--- Swappy/Entrypoint.cs
+++ Swappy/Entrypoint.cs
@@ -17,7 +17,7 @@ public class Entrypoint : Plugin<Config>
     public override string Name => "Swappy";
     public override string Description => "Your trusted companion for automatic plugin updates";
     public override string Author => "LilNesquuik";
-    public override Version Version => new(1, 0, 1);
+    public override Version Version => new(1, 0, 2);
     public override Version RequiredApiVersion => LabApiProperties.CurrentVersion;
     public override LoadPriority Priority => LoadPriority.Lowest;
 
