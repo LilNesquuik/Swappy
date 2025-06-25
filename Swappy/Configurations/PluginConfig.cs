024c2853669b437581f821c2b09a260720657985
Morgan Coyaux <zmentionb@gmail.com>
2025-06-25 02:56:25 +0200

    Add: Description

--- Swappy/Configurations/PluginConfig.cs
+++ Swappy/Configurations/PluginConfig.cs
@@ -1,15 +1,29 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace Swappy.Configurations;
 
 [Serializable]
 public class PluginConfig
 {
+    [Description("Name of the plugin (case sensitive)")]
     public string PluginName { get; set; }
+    
+    [Description("Owner of the GitHub repository")]
     public string? RepositoryOwner { get; set; }
+    
+    [Description("Name of the GitHub repository")] 
     public string? RepositoryName { get; set; }
+    
+    [Description("Optional GitHub access token for private repositories")]
     public string? AccessToken { get; set; }
+    
+    [Description("Whether to download and extract dependencies.zip from releases")]
     public bool DownloadDependencies { get; set; }
+    
+    [Description("Whether to check for updates when the plugin loads")]
     public bool UpdateOnStartup { get; set; }
-    public bool SoftRestart { get; set; }
+    
+    [Description("Whether to schedule a server restart after updating")]
+    public bool ScheduleSoftRestart { get; set; }
 }
